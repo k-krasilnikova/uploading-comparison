78d2421a5b0aed7743364e4668bd69ff49c50934
Katsiaryna <K.Krasilnikova@itechart-group.com>
2020-04-08 13:19:43 +0300

    Updates

---
+++ precache-manifest.47876ae56def6aaf597e4fbf6399a172.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12232a5f041ee77c1a8f5a644c128c96",
+    "url": "/uploading-comparison/index.html"
+  },
+  {
+    "revision": "cf624178d5cfb9d204dc",
+    "url": "/uploading-comparison/static/css/main.129a77be.chunk.css"
+  },
+  {
+    "revision": "067b9fff6f48e270ccb2",
+    "url": "/uploading-comparison/static/js/2.b6f63509.chunk.js"
+  },
+  {
+    "revision": "5d689addf2e0fd3f3b0cc224f7394b4d",
+    "url": "/uploading-comparison/static/js/2.b6f63509.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf624178d5cfb9d204dc",
+    "url": "/uploading-comparison/static/js/main.906ff4ff.chunk.js"
+  },
+  {
+    "revision": "92cf409791b68ad4a0e2",
+    "url": "/uploading-comparison/static/js/runtime-main.1b50fa62.js"
+  }
+]);
